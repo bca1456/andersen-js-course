5fa8adaccbed42fb9eb8328a7ef9f29365a6ab9d
Yauheni <eugene.antipenko.s@gmail.com>
2019-06-29 12:23:11 +0300

    Task 6 complete
    Task 6 complete

--- src/asynchronous-programming/6.js
+++ src/asynchronous-programming/6.js
@@ -4,3 +4,21 @@ function getResolvedPromise(value) {
   });
 }
 
+function exec(val) {
+  getResolvedPromise(val)
+    .then(function(value) {
+      if (value > 300) {
+        throw 'Ошибка';
+      }
+    })
+    .catch(function(e) {
+      console.log(e);
+    })
+    .finally(function() {
+      console.log('This is finally!');
+    });
+}
+
+export default function res6() {
+  exec(500);
+}
