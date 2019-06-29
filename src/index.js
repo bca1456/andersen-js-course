ec15c611d732e91d87707d8673f668f976662747
Yauheni <eugene.antipenko.s@gmail.com>
2019-06-29 11:05:57 +0300

    Task 4 complete
    Task 4 complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import res1 from './asynchronous-programming/1';
 import res2 from './asynchronous-programming/2';
 import res3 from './asynchronous-programming/3';
+import res4 from './asynchronous-programming/4';
 
 console.log('Hi');
 
 res1();
 res2();
 res3();
+res4();
